0d4fc4f2518484dc898748ded93674133fed4e6a
박민흠 (Min-Heum Park) <mhpark@markany.com>
2024-05-13 08:32:38 +0000

    대문자 디렉토리들 삭제

--- src/components/Pages/ErrorPage.tsx
+++
@@ -1,7 +0,0 @@
-import { Box } from '@mui/material';
-
-export default function ErrorPage() {
-  return (
-    <Box>error</Box>
-  );
-}
--- src/components/Pages/LoginPage.tsx
+++
@@ -1,5 +0,0 @@
-import { Box } from '@mui/material';
-
-export default function LoginPage() {
-  return <Box>loginPage</Box>
-}
