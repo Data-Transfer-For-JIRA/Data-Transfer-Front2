ee7719a645fb081c239ae332ef240f955e41c5b8
kimtaehun <kth_markany@naver.com>
2024-06-19 10:27:57 +0900

    스테이징 에러처리

---
+++ src/components/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import PageContents from "@organisms/PageContents";
+import MainPageTemplate from "@templates/MainPageTemplate";
+//TODO :  이부분에서 로그인에 대한 인증을 걸어두면 될듯.
+type MainPageType = {
+  fixFlag :boolean;
+}
+export default function MainPage({fixFlag =false}:MainPageType) {
+  return (
+    <MainPageTemplate>
+      <PageContents fixFlag={fixFlag}/>
+    </MainPageTemplate>
+  );
+}
